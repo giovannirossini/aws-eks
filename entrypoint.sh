809ad456bf65dc7ac7ed78e6929cf75803ebd413
Giovanni Rossini <giovannijrrossini@gmail.com>
2024-05-13 09:46:13 -0300

    Option to set Kubectl Version (#3)
    * Kubectl version
    
    * Kubectl version
    
    * fixing arch on curl
    
    * fixing arch on curl to amd64

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 set -e >> /dev/null 2>&1;
 set -o pipefail >> /dev/null 2>&1;
 
-alias kubectl="kubectl-v${VERSION:-1.30}"
+echo ">>> Setting up kubectl..."
+rm -rf /usr/bin/kubectl
+ln -s /usr/bin/kubectl-v${VERSION:-1.30} /usr/bin/kubectl
 
 echo ">>> Authenticating..."
 
